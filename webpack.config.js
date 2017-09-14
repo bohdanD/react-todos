49b453a1cbdf591949802e754e6ac77407324fc6
Bohdan Dorosh <doroshbohdan1@gmail.com>
2017-09-14 18:11:17 +0300

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 var path = require("path");
 module.exports = {
     entry:{
-        app: ["index.js"]
+        app: ["./index.js"]
     },
     output: {
         path: path.resolve(__dirname),
         publicPath: "/assets/",
         filename: "bundle.js"
+    },
+    module:{
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel",
+        query: {
+            presets:['react']
+        }
     }
 };
